d4884294a25cd6d95fbfd2245a6b75d299f95c7b
yuda1124 <yuda1124@naver.com>
2020-08-02 00:35:53 +0900

    [#36] refactor: 추가 - alias
    - @util, @validator, @controller 추가

--- server/api/routes/record-reload.js
+++ server/api/routes/record-reload.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-import { wrapAsync } from '../../util';
-import { isReloadInMonth } from '../../controller/record-reload';
-import { validateRecordReloadParameter } from '../../validator/record';
+import { wrapAsync } from '@util';
+import { isReloadInMonth } from '@controller/record-reload';
+import { validateRecordReloadParameter } from '@validator/record';
 
 router.get(
   '/:year/:month',
--- server/api/routes/record.js
+++ server/api/routes/record.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-import { wrapAsync } from '../../util';
-import { getRecordInMonth } from '../../controller/record';
-import { validateRecordParameter } from '../../validator/record';
+import { wrapAsync } from '@util';
+import { getRecordInMonth } from '@controller/record';
+import { validateRecordParameter } from '@validator/record';
 
 router.get(
   '/:year/:month',
