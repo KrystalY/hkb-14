b35308a099174b34b07a3d263f954bcdd12dcc4e
Wudy <wudy343@gmail.com>
2020-08-03 21:51:27 +0900

    chore: 제거 - Model 내 주석
    필요없는 주석 제거

--- client/src/model/model.js
+++ client/src/model/model.js
@@ -3,14 +3,3 @@ import { Store } from '@constant/Store.js';
 export default class Model {
   constructor() {}
 }
-
-// const router = new Router();
-// const model = new Model();
-
-// // Page
-// notify(RouterEvent.Action, { action: 'move', path: '/' });
-
-// // Router
-// subscribe(router, RouterEvent.Action, onReceivedAction);
-
-// router(path, parameter) < -subscribe, notify;
