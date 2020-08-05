5880f2f1016f831171b304b6331bd5fc8f1c4382
Wudy <wudy343@gmail.com>
2020-08-05 21:59:21 +0900

    chore: 추가 - 웹팩 production 빌드 옵션 (uglifyjs)
    production 빌드 때, console, comment 제거하고 배포하는 옵션이 추가됨

--- client/webpack.config.production.js
+++ client/webpack.config.production.js
@@ -1,7 +1,22 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'hidden-source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: true,
+          },
+          output: {
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
 });
